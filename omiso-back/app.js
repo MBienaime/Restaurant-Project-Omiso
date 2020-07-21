b7c56d0b5930cac9c69bb0b38631c2b6dff50d48
FabDen <fabden@gmail.com>
2020-07-21 21:17:02 +0200

    HTTPS server with a self-signed certificate

--- omiso-back/app.js
+++ omiso-back/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use('/', express.static(__dirname + '/public'));
 
 app.use((req, res, next)=> {
     res.status(200).json({
--- omiso-back/server.js
+++ omiso-back/server.js
@@ -1,9 +1,13 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('./Config/Certificates/Keyomiso.key'),
+    cert: fs.readFileSync('./Config/Certificates/omiso.crt')
+  };
 
 
-const port = process.env.PORT || 8080;
+const server = https.createServer(options,app);
 
-const server = http.createServer(app);
-
-server.listen(port);
+server.listen(443, console.log('Server listen port: 433'));
