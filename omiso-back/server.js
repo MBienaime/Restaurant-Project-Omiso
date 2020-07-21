44a48aca000717324eec51c28758c3156fd36271
FabDen <fabden@gmail.com>
2020-07-21 22:44:58 +0200

    Configuring unit tests with Mocha and Chai

--- omiso-back/server.js
+++ omiso-back/server.js
@@ -1,13 +1,16 @@
+//import
 const https = require('https');
 const app = require('./app');
 const fs = require('fs');
 
+
+// Certificates
 const options = {
     key: fs.readFileSync('./Config/Certificates/Keyomiso.key'),
     cert: fs.readFileSync('./Config/Certificates/omiso.crt')
   };
 
 
+//Server listen
 const server = https.createServer(options,app);
-
-server.listen(443, console.log('Server listen port: 433'));
+server.listen(443, console.log('Server listen port: 443'));
---
+++ omiso-back/test/test.js
@@ -0,0 +1,8 @@
+//import
+const chai = require('chai');
+const chaihttp = require ('chai-http');
+const app = require('../app');
+const expect  = require('chai').expect;
+
+chai.use(chaihttp);
+
