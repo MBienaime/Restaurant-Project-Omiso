454783c62eeaf3a09dfb4368147a47dc70d7e045
FabDen <fabden@gmail.com>
2020-07-22 20:48:54 +0200

    Mongoose installation + successful connection to database

--- omiso-back/app.js
+++ omiso-back/app.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 //import route
 const OrderRoute = require('./Routes/OrderRoute');
 
+
+//connection Data
+
+mongoose.connect('mongodb://localhost:27017/omiso',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 // Cors
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
+*/
 // body-parser
 
 app.use(express.urlencoded({ extended: true }));
