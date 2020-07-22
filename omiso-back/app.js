ee844619432973fa4e75e5464c48d01bd6c1ca50
FabDen <fabden@gmail.com>
2020-07-22 18:58:41 +0200

    Order Route Creation + test unitaire ok

--- omiso-back/app.js
+++ omiso-back/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
+//import route
+const OrderRoute = require('./Routes/OrderRoute');
 
+// Cors
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+// body-parser
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Route
 app.use('/', express.static(__dirname + '/public'));
-
-app.use((req, res, next)=> {
-    res.status(200).json({
-        message: 'Currently running'
-    });
-});
+app.use('/order', OrderRoute);
 
 module.exports = app; 
--- omiso-back/test/test.js
+++ omiso-back/test/test.js
@@ -6,3 +6,48 @@ const expect  = require('chai').expect;
 
 chai.use(chaihttp);
 
+describe('apiomiso',()=>{
+    describe('# Test Route Order',()=>{
+
+        it('should list ALL order',(done)=>{
+            chai.request(app)
+                .get('/order')
+                .end((err,res)=>{
+                    expect(res).to.have.status(200);
+                    expect(err).to.be.null;
+                });
+                done()
+        });
+
+        it('should post one order',(done)=>{
+            chai.request(app)
+                .post('/order')
+                .end((err,res)=>{
+                    expect(res).to.have.status(200);
+                    expect(err).to.be.null;
+                });
+                done()
+        });
+
+        it('should delete one order',(done)=>{
+            chai.request(app)
+                .get('/order/1')
+                .end((err,res)=>{
+                    expect(res).to.have.status(200);
+                    expect(err).to.be.null;
+                });
+                done()
+        });
+
+        it('should list one order',(done)=>{
+            chai.request(app)
+                .get('/order/1')
+                .end((err,res)=>{
+                    expect(res).to.have.status(200);
+                    expect(err).to.be.null;
+                });
+                done()
+        });
+
+    })
+})
