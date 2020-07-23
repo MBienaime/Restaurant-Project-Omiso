9b08c4d17a455f696eff9d038f70bf93a8b5e58c
myriam AB <myriam.bienaime1@gmail.com>
2020-07-23 16:47:50 +0200

    Menu item Model created

---
+++ omiso-back/Models/MenuItemModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const menuItemSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    name : String,
+    description : String,
+    price: Number,
+    category : String,
+    quantity : Number,
+    status : Boolean
+});
+
+module.exports = mongoose.model('MenuItem', menuItemSchema);
