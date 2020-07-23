ac8ff32fd7403e4580f2f0de87d2ad1bc1402411
myriam AB <myriam.bienaime1@gmail.com>
2020-07-23 21:55:51 +0200

    Adding comments in code

--- omiso-back/Models/MenuItemModel.js
+++ omiso-back/Models/MenuItemModel.js
@@ -1,5 +1,7 @@
+//Import
 const mongoose = require("mongoose");
 
+//Schema defines the shape of the documents in the dataBase collection
 const menuItemSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
@@ -10,4 +12,6 @@ const menuItemSchema = mongoose.Schema({
   status: Boolean,
 });
 
+
+//Converts menuItemSchema into a Model + exports it
 module.exports = mongoose.model("MenuItem", menuItemSchema);
