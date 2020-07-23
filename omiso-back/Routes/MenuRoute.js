ec31b14cbae9db96ff40f25cc7e781e993096089
myriam AB <myriam.bienaime1@gmail.com>
2020-07-23 08:12:22 +0200

    New routes creation GET menu by id + PATCH + DELETE

--- omiso-back/Routes/MenuRoute.js
+++ omiso-back/Routes/MenuRoute.js
@@ -1,18 +1,42 @@
-const express = require('express'); 
+const express = require("express");
 
-const router = express.Router(); 
+const router = express.Router();
 
-
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message : 'GET request to /menu'
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "GET request to /menu",
+  });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message : 'POST request to /menu'
+router.post("/", (req, res, next) => {
+  res.status(201).json({
+    message: "POST request to /menu",
+  });
+});
+
+router.get("/:menuId", (req, res, next) => {
+  const id = req.params.menuId;
+  if (id === "1") {
+    res.status(200).json({
+      message: "GET request to menu/1",
     });
+  } else {
+    res.status(200).json({
+      message: "new ID passed",
+    });
+  }
+});
+
+router.patch("/:menuId", (req, res, next) => {
+  res.status(200).json({
+    message: "Updated menu",
+  });
+});
+
+router.delete("/:menuId", (req, res, next) => {
+  res.status(200).json({
+    message: "Deleted menu",
+  });
 });
 
 module.exports = router;
