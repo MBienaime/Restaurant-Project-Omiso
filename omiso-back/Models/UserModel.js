c8d84f7f658860f19966d0ef406dfcadf18e1a2b
myriam AB <myriam.bienaime1@gmail.com>
2020-07-24 20:52:41 +0200

    Improving the regular expression

--- omiso-back/Models/UserModel.js
+++ omiso-back/Models/UserModel.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    match: /[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
   },
   password: { type: String, required: true },
   lastname: { type: String, required: true },
