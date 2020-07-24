1de74ced248e4719685c0b4ce0633b7c5e8aeff6
myriam AB <myriam.bienaime1@gmail.com>
2020-07-24 16:55:48 +0200

    Separating routes logic into controllers

--- omiso-back/Routes/MenuRoute.js
+++ omiso-back/Routes/MenuRoute.js
@@ -1,97 +1,21 @@
+//Imports
 const express = require("express");
-const mongoose = require("mongoose");
 const router = express.Router();
-
-const MenuItem = require("../Models/MenuItemModel");
+const MenuController = require("../Controllers/MenuController");
 
 // Find all menu items
-router.get("/", (req, res, next) => {
-  MenuItem.find()
-    .exec()
-    .then((docs) => {
-      res.status(200).json(docs);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.get("/", MenuController.menuItems_get_all);
 
 // Create new menu item
-router.post("/", (req, res, next) => {
-  const menuItem = new MenuItem({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    category: req.body.category,
-    quantity: req.body.quantity,
-    status: req.body.status,
-  });
-
-  // Save MenuItem in the database
-  menuItem
-    .save()
-    .then((result) => {
-      res.status(201).json({
-        message: "POST request to /menuItem",
-        createdMenuItem: result,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-});
+router.post("/", MenuController.menuItems_create_item);
 
 // Find menu item by id
-router.get("/:menuItemId", (req, res, next) => {
-  const id = req.params.menuItemId;
-  MenuItem.findById(id)
-    .exec()
-    .then((doc) => {
-      res.status(200).json(doc);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-});
+router.get("/:menuItemId", MenuController.menuItems_get_item);
 
 // Update an existing menu item by its id
-router.patch("/:menuItemId", (req, res, next) => {
-  const id = req.params.menuItemId;
-  const updateOps = {};
-
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-
-  MenuItem.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then((result) => {
-      res.status(200).json({ message: "Product updated" });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.patch("/:menuItemId", MenuController.menuItems_update_item);
 
 // Delete menu item by id
-router.delete("/:menuItemId", (req, res, next) => {
-  const id = req.params.menuItemId;
-  MenuItem.remove({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.delete("/:menuItemId", MenuController.menuItems_delete_item);
 
 module.exports = router;
