50cbb9d41c40a3f25eaa34631c182a019ed58298
FabDen <fabden@gmail.com>
2020-07-27 19:21:55 +0200

    Maj update Route by id

--- omiso-back/Controllers/OrderController.js
+++ omiso-back/Controllers/OrderController.js
@@ -37,28 +37,36 @@ exports.deleteOrder = (req, res) => {
 //Get Order By id
 
 exports.getOrderById = (req,res)=>{
-  Order.findById(req.params.id,(err,doc)=>{
-    if (err){res.status(404).json({ error: "Menu no valid"})}
-      else{  
-        res.status(200).json(doc);
-      }
-        
-})
-}
+  Order.findById(req.params.id)
+  .exec()
+  .then((doc)=>{res.status(200).json(doc)})
+  .catch((err)=>{res.status(404).json({ error: err})})
+
+};
 
 //Update Order by id
-
+/*
 exports.updateOrderById = (req, res) => {    
   Order.findById(req.params.id,(err,doc)=>{
     if (err){res.status(404).json({ error: err})}
-      else{  
-
+      else{
       doc.idUser= req.body.idUser;
-      doc.OrderMenu= req.body.OrderMenu;
-      
+      doc.OrderMenu= req.body.OrderMenu;      
       doc.save()
         .then((docs) => { res.status(200).json(doc); })
         .catch((err) => { res.status(500).json({ error: err})});
       }
   })   
+}; */
+
+exports.updateOrderById = (req, res) => {
+  Order.findById(req.params.id)
+  .then((doc)=>{
+    doc.idUser= req.body.idUser;
+    doc.OrderMenu= req.body.OrderMenu;      
+    doc.save()
+      .then((doc) => { res.status(200).json(doc); })
+      .catch((err) => { res.status(500).json({ error: err})});
+  })
+  .catch((err)=>{res.status(404).json({ error: err})})
 };
