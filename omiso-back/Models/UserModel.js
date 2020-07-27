5642adb58cd2b2299959ca8a316ac875c5f781dd
myriam AB <myriam.bienaime1@gmail.com>
2020-07-27 12:31:44 +0200

    Delete user route

--- omiso-back/Models/UserModel.js
+++ omiso-back/Models/UserModel.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   email: { type: String, required: true },
   password: { type: String, required: true },
+
 });
 
 module.exports = mongoose.model("User", userSchema);
--- omiso-back/Routes/UserRoute.js
+++ omiso-back/Routes/UserRoute.js
@@ -48,4 +48,21 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+
+//Delete user by its id
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "User deleted"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
 module.exports = router;
