49319e33bb5273937da2eec0ec83bccda03d75c4
FabDen <fabden@gmail.com>
2020-07-27 20:15:59 +0200

    maj route update by id

--- omiso-back/Controllers/OrderController.js
+++ omiso-back/Controllers/OrderController.js
@@ -44,21 +44,6 @@ exports.getOrderById = (req,res)=>{
 
 };
 
-//Update Order by id
-/*
-exports.updateOrderById = (req, res) => {    
-  Order.findById(req.params.id,(err,doc)=>{
-    if (err){res.status(404).json({ error: err})}
-      else{
-      doc.idUser= req.body.idUser;
-      doc.OrderMenu= req.body.OrderMenu;      
-      doc.save()
-        .then((docs) => { res.status(200).json(doc); })
-        .catch((err) => { res.status(500).json({ error: err})});
-      }
-  })   
-}; */
-
 exports.updateOrderById = (req, res) => {
   Order.findById(req.params.id)
   .then((doc)=>{
