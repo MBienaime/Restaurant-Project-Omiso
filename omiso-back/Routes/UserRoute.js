df06d5fdc06d94cefa91f92a66acc94baa05f153
myriam AB <myriam.bienaime1@gmail.com>
2020-07-27 06:28:10 +0200

    checking and testing if email already exists

--- omiso-back/Routes/UserRoute.js
+++ omiso-back/Routes/UserRoute.js
@@ -1,4 +1,4 @@
-//Import 
+//Import
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -7,34 +7,45 @@ const bcrypt = require("bcrypt");
 // User model
 const User = require("../Models/UserModel");
 
-
 //Sign Up route : creates a new user
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
-        error: err,
-      });
-    } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then((result) => {
-          res.status(201).json({
-            message: "User created",
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            error: err,
-          });
+
+// Checking if email already exists
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "This email already exists",
         });
-    }
-  });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                res.status(201).json({
+                  message: "User created",
+                });
+              })
+              .catch((err) => {
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
 });
 
 module.exports = router;
