9f7b8f3ddc5adf3b82938712c043b860859dbd7f
myriam AB <myriam.bienaime1@gmail.com>
2020-07-27 12:35:11 +0200

    Regular expression email validation

--- omiso-back/Models/UserModel.js
+++ omiso-back/Models/UserModel.js
@@ -1,10 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true }
 });
 
-module.exports = mongoose.model("User", userSchema);
+//resolves deprecation warning
+mongoose.set('useCreateIndex', true);
+
+module.exports = mongoose.model('User', userSchema);
