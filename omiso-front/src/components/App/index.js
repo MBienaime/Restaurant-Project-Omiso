c1ba0eabc2b0221ec9e94f6049f1425a4edadab5
MBienaime <lmyriam.ab@gmail.com>
2020-08-13 16:32:07 +0200

    webpackConfig modification

--- omiso-front/src/components/App/index.js
+++ omiso-front/src/components/App/index.js
@@ -1,95 +1,14 @@
 // == Import npm
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-
-// == Import
-import Footer from '../Footer';
-import Header from '../Header';
-import Main from '../Main';
-import SectionMenu from '../../containers/SectionMenu';
-import Inscription from '../Modal/Inscription';
-import Connexion from '../Modal/Connexion';
-import OrderValidation from '../Modal/OrderValidation';
-import OrderManagementPage from '../../containers/OrderManagementPage';
 
 // == Import Style
 import './styles.scss';
 
 // commande recuperation API
 // == Import npm
-const App = ({
-  fetchItems,
-  offModalConnexion,
-  onModalInscription,
-  offModalInscription,
-  offModalOrder,
-  userLogin,
-  userInscription,
-  showModal,
-  userIsLogged,
-  checkAuthentification,
-  sendOrders,
-}) => {
-  useEffect(fetchItems, []);
- 
-  useEffect(checkAuthentification, []);
-
-  // systeme de route affichage
-
-
-  return (
-    <div className="app">
-      <Header userIsLogged={userIsLogged} />
-      <Inscription
-        showModal={showModal.showModalInscription}
-        offModalInscription={offModalInscription}
-        userInscription={userInscription}
-      />
-      <Connexion
-        showModal={showModal.showModalConnexion}
-        offModalConnexion={offModalConnexion}
-        onModalInscription={onModalInscription}
-        userLogin={userLogin}
-      />
-
-
-      {!userIsLogged.isAuthUser && (<Main />)}
-
-      {userIsLogged.isAuthUser ? (
-        userIsLogged.role === 'ROLE_EMPLOYEE' ? (
-          <OrderManagementPage />
-        ) : (
-          <SectionMenu />
-        )
-      ) : (
-
-        <SectionMenu />
-
-      )}
-
-      <OrderValidation showModal={showModal.showModalOrder} offModalOrder={offModalOrder} sendOrders={sendOrders} />
-
-
-      <Footer userIsLogged={userIsLogged}/>
-    </div>
-
-  );
-};
-
-App.propsTypes = {
-  fetchItems: PropTypes.func.isRequired,
-  offModalConnexion: PropTypes.func.isRequired,
-  onModalInscription: PropTypes.func.isRequired,
-  offModalInscription: PropTypes.func.isRequired,
-  userLogin: PropTypes.func.isRequired,
-  receiveOrder: PropTypes.func.isRequired,
-  userInscription: PropTypes.func.isRequired,
-  showModal: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.object,
-    PropTypes.func,
-  ]),
-};
+const App = () =>(
+  <div>test</div>
+)
 
 // == Export
 export default App;
--- omiso-front/src/index.js
+++ omiso-front/src/index.js
@@ -1,27 +1,13 @@
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 
 // == Import : local
 // Composants
-import App from 'src/containers/App';
-// Store
-import store from 'src/store';
+import App from 'src/components/App';
 
+const rootReactElement = <App />;
 
-// == Render
-// 1. Le composant racine (celui qui contient l'ensemble de l'app)
-const rootComponent = (
-  <Provider store={store}>
-    <Router >
-      <App history={ Router.history }/>
-    </Router>
-  </Provider>
-);
-// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
-// Le rendu de React => DOM
-render(rootComponent, target);
+
+render(rootReactElement, target);
