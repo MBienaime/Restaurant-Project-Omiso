687ecdd21f55d9c872ad685065ec9b974b196b18
FabDen <fabden@gmail.com>
2020-08-17 00:22:44 +0200

    schema order modification for populate

--- omiso-back/Routes/OrderRoute.js
+++ omiso-back/Routes/OrderRoute.js
@@ -1,7 +1,7 @@
 // import
 
 const express = require('express');
-const checkAuth = require("../Middleware/CheckAuth");
+const checkAuth = require('../Middleware/CheckAuth');
 const checkRoles = require('../Middleware/CheckRoles');
 
 const OrderRoute = express.Router();
@@ -13,12 +13,12 @@ const OrderController = require('../Controllers/OrderController');
 // Order Route
 
 OrderRoute.route('/')
-  .get(checkAuth,checkRoles(["admin","employé"]),OrderController.getOrder)
-  .post(checkAuth,checkRoles(["admin"]),OrderController.postOrder);
+  .get(checkAuth, checkRoles(['admin', 'employé']), OrderController.getOrder)
+  .post(checkAuth, checkRoles(['admin']), OrderController.postOrder);
 
-OrderRoute.route('/:id',checkAuth)
-  .delete(checkAuth,checkRoles(["admin"]),OrderController.deleteOrder)
-  .get(checkAuth,checkRoles(["admin","employé"]),OrderController.getOrderById)
-  .patch(checkAuth,checkRoles(["admin","employé"]),OrderController.updateOrderById); 
+OrderRoute.route('/:id', checkAuth)
+  .delete(checkAuth, checkRoles(['admin']), OrderController.deleteOrder)
+  .get(checkAuth, checkRoles(['admin', 'employé']), OrderController.getOrderById)
+  .patch(checkAuth, checkRoles(['admin', 'employé']), OrderController.updateOrderById);
 
 module.exports = OrderRoute;
