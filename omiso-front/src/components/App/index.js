1ac30a504f77e6148050922904076f5dbe89e848
MBienaime <lmyriam.ab@gmail.com>
2020-08-17 15:27:05 +0200

    successful connection to API

--- omiso-front/src/components/App/index.js
+++ omiso-front/src/components/App/index.js
@@ -2,22 +2,40 @@
 import React from 'react';
 import P404 from '../404';
 import Panier from '../Panier';
-import Menuitem from '../Menuitem'
-import Home from '../Home'
+import Menuitem from '../Menuitem';
+import Home from '../Home';
+import {useState} from 'react';
+import axios from 'axios';
 
 // == Import Style
 import './styles.scss';
 
+
+
 // commande recuperation API
 // == Import npm
-const App = () =>(
+const App = () => {
+  
+  const [data, setData] = useState([]);
+  
+    const url = `https://omiso.com/menu/`;
+    axios.get(url)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });  
+
+  return (
   <>
    <Home/>
     <P404/>    
     <Menuitem/>
     <Panier/>
   </>
-)
+  )
+}
 
 // == Export
 export default App;
