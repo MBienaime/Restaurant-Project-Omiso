8c7bc75af95dc7c33a565e1060d7033fa7ebe90d
MBienaime <lmyriam.ab@gmail.com>
2020-08-17 12:53:04 +0200

    home-page

---
+++ omiso-front/src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaShoppingCart } from 'react-icons/fa';
+// == Import Style
+import "./style.css";
+
+// == Import npm
+const Header = () => (
+  <nav className="navbar">
+
+    <div className="nav_logo"></div>
+
+    <div className = "nav_links">
+    <a className="nav_link" href="#"> Accueil</a>
+    <a className="nav_link" href="#"> Contact </a>
+    <a className="nav_link" href="#"> Connexion </a>
+    </div>
+    <div class="cart"><FaShoppingCart/></div>
+  </nav>
+);
+// == Export
+export default Header;
--- omiso-front/src/components/Home/index.js
+++ omiso-front/src/components/Home/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaFacebookF, FaTwitter, FaInstagram, FaTripadvisor } from 'react-icons/fa';
+// Local import 
+import Header from "../Header";
 
 // == Import Style
 import "./style.css";
@@ -7,29 +10,31 @@ import "./style.css";
 // == Import npm
 const Home = () => (
   <div className="home">
-    <div class="introduction scale-in-hor-left">
-      <div class="intro_text ">
-        <h1>Click and Collect</h1>
-        <p>
-          <span> 1 </span>Parcourez notre carte et choisissez parmis nos
-          produits.
-        </p>
-        <p>
-          <span> 2 </span>Ajoutez votre sélection au panier puis valider.
-        </p>
-        <p>
-          <span> 3 </span>Connectez vous pour régler votre achat.
-        </p>
-        <p>
-          <span> 4 </span>Récupérez votre commande sur place.
-        </p>
-      </div>
-  
-        <button class="btn_menu">Notre carte</button>
-    
-    </div>
+  <Header/>
+   <div className="presentation ">
+   
+  <div className="social_icons">
+     <a href="#" className="icon_link"><FaFacebookF className ="link_icons"/></a>
+     <a href="#" className="icon_link"><FaTwitter className ="link_icons"/></a>
+      <a href="#" className="icon_link"><FaInstagram className ="link_icons"/></a>
+     <a href="#" className="icon_link"><FaTripadvisor className ="link_icons"/></a>
   </div>
-);
 
+
+
+<div className="introduction scale-in-hor-left">
+    <div className="intro_text ">
+        <h1>Click and Collect</h1>
+       <p><span> 1  </span>Parcourez notre carte et choisissez parmis nos produits.</p>
+        <p><span> 2  </span>Ajoutez votre sélection au panier puis valider.</p>
+        <p><span> 3  </span>Connectez vous pour régler votre achat.</p>
+        <p><span> 4  </span>Récupérez votre commande sur place.</p>
+    </div>
+ 
+        <button className="btn_menu">Notre carte</button>
+</div>
+</div>
+</div>
+)
 // == Export
 export default Home;
