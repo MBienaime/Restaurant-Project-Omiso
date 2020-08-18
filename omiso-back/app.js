5b01e0a1d46a841cc701736ab429ba5d453e506a
MBienaime <66524191+MBienaime@users.noreply.github.com>
2020-08-18 15:13:32 +0200

    handling cors

--- omiso-back/app.js
+++ omiso-back/app.js
@@ -20,7 +20,12 @@ mongoose
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  
+  app.use ((req, res, next) => {
+    res.setHeader ('Access-Control-Allow-Origin', '*');
+    res.setHeader ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    suivant();
+  });
 
 // Admin-bro : route to administration panel
 app.use('/admin', adminRouter);
