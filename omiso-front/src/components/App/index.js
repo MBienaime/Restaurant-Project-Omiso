e752048a8201c126156fff97a40a2652ef8f1d6c
MBienaime <66524191+MBienaime@users.noreply.github.com>
2020-08-19 12:06:00 +0200

    code organization

--- omiso-front/src/components/App/index.js
+++ omiso-front/src/components/App/index.js
@@ -3,72 +3,27 @@ import React from 'react';
 import axios from 'axios';
 import {useState, useEffect } from 'react';
 
+// == Import Style
+import './styles.scss';
 
 // Local imports 
 import Home from '../Home';
 import MenuItems from '../MenuItems';
-import Connection from '../Connection';
-
-// == Import Style
-import './styles.scss';
+import Header from'../Header';
 
 
 
-// Getting data from API
 const App = () => {
-  
-  const [data, setData] = useState([]);  
-  const [ModalConnexion, setModalConnexion] = useState(false);
-
- 
-
-
-  //API call
-    const getApiData = () => {
-      const url = `https://omiso.com/menu/`;
-      axios.get(url)
-      .then((resp) => {
-        setData(resp.data.menuItems)
-      })
-      .catch((error) => {
-        console.log('error', error);
-      });  
-    }
-
-    
-//getting menu data
-    useEffect(getApiData, []);    
-
-//modal connexion
-   const showModalConnexion = () => {
-      setModalConnexion(true);
-    };
-  
-   const hideModalConnexion = () => {
-      setModalConnexion(false);
-     };
 
   return (
 <>
-   <Home showModalConnexion={showModalConnexion}/> 
 
-   {
-    ModalConnexion &&
-   <Connection hideModalConnexion={hideModalConnexion}/>
-   }
+<Header/> 
+<Home/> 
+<MenuItems/> 
+</>
 
-   <div className="sectionMenu">
-     { data.map( (d) =>(<MenuItems 
-        title = {d.name}
-        description = {d.description}
-        price = {d.price}
-        Image = ''
-        key={d._id}
-     />)) } 
-     </div>  
- </>
-  )
-}
+)}
 
 // == Export
 export default App;
--- omiso-front/src/components/MenuItems/index.js
+++ omiso-front/src/components/MenuItems/index.js
@@ -1,38 +1,62 @@
 // == Import npm
 import React from 'react';
-import PropTypes from 'prop-types';
+import {useState, useEffect } from 'react';
+import axios from 'axios';
 
 // == Import Style
 import './styles.css';
 
 // commande recuperation API
 // == Import npm
-const Menuitems = ({title, description, price, image}) =>(
+const Menuitems = ( ) =>{
+
+const [data, setData] = useState([]);   
 
 
-    <div className="menuitem">
+  //API call
+ const getApiData = () =>{
+    const url = `https://omiso.com/menu/`;
+    axios.get(url)
+    .then((resp) => {
+      setData(resp.data.menuItems)
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });  
+  };
+  
+  //getting menu data
+  useEffect(getApiData, []) ; 
+
+return(
+
+<div className="sectionMenu">
+{ 
+data.map( (data) =>(
+    <div className="menuitem" key={data._id}>
         <div className="menuitem-image"></div>
         <div className="menutitem-animate">
             <div className="menutitem-animate-transparent"></div>
             <div className="menutitem-animate-description">
                 <div className="flex border-bottom">
-                <h1 className="title ">{title} </h1>
-                    <p className="title ">{price}€</p>
+                <h1 className="title "> {data.title}</h1>
+                    <p className="title ">{data.price}€</p>
                 </div>
-                <p className=' descrition'>{description}</p>            
+                <p className=' descrition'>{data.description}</p>            
                 <div className='flex '>
                 <button className=" button ">Ajouter</button>
                 </div>
             </div>
         </div>
     </div>
+))} 
+</div>
 
-)
-Menuitems.propTypes = { 
-    title : PropTypes.string.isRequired,
-    price : PropTypes.number.isRequired,
-    description :  PropTypes.string.isRequired
-}
+
+
+)}
 
 // == Export
 export default Menuitems;
+
+    
