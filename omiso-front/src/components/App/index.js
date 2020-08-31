152d779d627597ed0be0cbc07b9130c32efbde19
MBienaime <66524191+MBienaime@users.noreply.github.com>
2020-08-31 19:44:47 +0200

    render App test

--- omiso-front/src/components/App/index.js
+++ omiso-front/src/components/App/index.js
@@ -1,65 +1,71 @@
 // == Import npm
-import React, {useState,} from 'react';
-import { Route, Switch } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
 
 // == Import Style
-import './styles.scss';
+import "./styles.scss";
 
-// Local imports 
-import Home from '../Home';
-import MenuItems from '../MenuItems';
-import Header from '../Header';
-import Cart from '../Cart';
-import Connection from '../Connection';
-import AdminPanel from '../AdminPanel/index';
-
-
+// Local imports
+import Home from "../Home";
+import MenuItems from "../MenuItems";
+import Header from "../Header";
+import Cart from "../Cart";
+import Connection from "../Connection";
+import AdminPanel from "../AdminPanel/index";
 
 const App = () => {
+  const [useorder, setorder] = useState([]);
 
-const [useorder, setorder] = useState([]);
+  const addOrder = (d) => {
+    if (!useorder.some((e) => e._id === d._id)) {
+      setorder([...useorder, { ...d, quantity: 1 }]);
+    } else {
+      const newdata = useorder.map((e) =>
+        e._id === d._id ? { ...e, quantity: e.quantity + 1 } : { ...e }
+      );
+      setorder(newdata);
+    }
+  };
 
-const addOrder = (d)=>{ 
-if (!useorder.some((e)=>e._id === d._id)){ setorder([...useorder,{...d, quantity:1}]) }
-else{ const newdata = useorder.map((e)=>((e._id===d._id)?({...e,quantity:e.quantity+1,}):({...e,}))); setorder(newdata); };
-}
+  const RemoveOrder = (d) => {
+    if (!useorder.some((e) => e._id === d._id)) {
+      setorder([...useorder, { ...d, quantity: 1 }]);
+    } else {
+      const newdata = useorder.map((e) =>
+        e._id === d._id ? { ...e, quantity: e.quantity - 1 } : { ...e }
+      );
+      setorder(newdata);
+    }
+  };
 
-const RemoveOrder = (d)=>{
-if (!useorder.some((e)=>e._id === d._id)){ setorder([...useorder,{...d, quantity:1}]) }
-else{ const newdata = useorder.map((e)=>((e._id===d._id)?({...e,quantity:e.quantity-1,}):({...e,}))); setorder(newdata); };
-}
+  //selector order menu
+  const usefilterorder = useorder.filter((e) => e.quantity > 0);
 
-//selector order menu
-const usefilterorder= useorder.filter((e)=>(e.quantity>0));
-
-
-return (
-<>
-<Header/> 
-<Home/>
-<Switch>
+  return (
+    <>
+      <Header />
+      <Home />
+      <Switch>
         <Route exact path="/">
-            <MenuItems addOrder={addOrder} /> 
-          </Route>
-          <Route path="/Connexion">
-            <Connection />
-          </Route>
-          <Route path="/Panier">
-            <Cart  DataOrder={usefilterorder} addOrder={addOrder} RemoveOrder={RemoveOrder}/>
-          </Route>
-</Switch>
-{/*<CardMenus/>*/ }
+          <MenuItems addOrder={addOrder} />
+        </Route>
+        <Route path="/Connexion">
+          <Connection />
+        </Route>
+        <Route path="/Panier">
+          <Cart
+            DataOrder={usefilterorder}
+            addOrder={addOrder}
+            RemoveOrder={RemoveOrder}
+          />
+        </Route>
+      </Switch>
+      {/*<CardMenus/>*/}
 
-
-<AdminPanel/>
-</>
-
-)}
-
+      <AdminPanel />
+    </>
+  );
+};
 
 // == Export
 export default App;
-
-
-
