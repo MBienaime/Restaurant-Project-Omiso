707edf7890dbbb42100b5cbeda56846688cfe7f6
FabDen <fabden@gmail.com>
2020-09-01 17:30:55 +0200

    add connection axios

--- omiso-front/src/components/SectionMenu/index.js
+++ omiso-front/src/components/SectionMenu/index.js
@@ -1,39 +1,50 @@
 // == Import npm
-import React from 'react';
-import ItemMenu from './ItemMenu/ItemMenu'
+import React,{useState,useEffect} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import ItemMenu from './ItemMenu/ItemMenu';
 import Footer from '../Footer';
 
 // == Import Style
 import './styles.css';
 
 const SectionMenu = () =>{
+        
+    const [data, setData] = useState([{_id:""}]);  
+    const [category, setCategory] =useState("");
+
+  //API call data menu
+  const getApiData = () =>{
+    const url = `https://omiso.com/menu/`;
+        axios.get(url)
+        .then((resp) => {
+          const addquantity = resp.data.menuItems.map((e)=>({...e,quantity:0}))
+          setData(addquantity)
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });  
+      };
+      
+      //getting menu data
+      useEffect(getApiData, []) ;
+
+      const filterCategory = (data, category)=>{if(!category){return data }else {return (data.filter((e)=>(e.category===category)))}};
 
 
 return(
 
 <div className="sectionMenus">
         <div className="sectionMenusCarteMenu">
-<ul><li>Entree</li><li>Plats</li><li>Dessert</li><li>Boisson</li></ul>
+<ul><li onClick={()=>(setCategory('Entree'))}>Entree</li><li onClick={()=>(setCategory('Plat'))}>Plats</li><li onClick={()=>(setCategory('Dessert'))}>Dessert</li><li onClick={()=>(setCategory('Boisson'))}>Boisson</li></ul>
         </div>
     <div className='sectionMenuCarte'>
-
+{ 
+filterCategory(data,category).map( (data) =>(<ItemMenu data={data}></ItemMenu>))
+}
         
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-            <ItemMenu></ItemMenu>
-        
+            
+   
     </div>
     <Footer/>
 </div>
