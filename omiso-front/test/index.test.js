0f8126fac2d6d91750b4906bfc312ec3dfe8f48d
FabDen <fabden@gmail.com>
2020-09-17 22:34:32 +0200

    fix bug pakage.json

--- omiso-front/test/index.test.js
+++ omiso-front/test/index.test.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import expect from 'chai';
 import { shallow } from 'enzyme';
-
-
-
-
-describe.skip('test simple',()=>{
-
-})
-
+import App from '../src/components/App';
+import Header from '../src/components/Header';
 
 describe('rendering components', () => {
-  it('renders App without crashing',() => {
-    shallow(<App/>);
+  it('renders App without crashing', () => {
+    shallow(<App />);
   });
   it('renders Header component without crashing', () => {
-    const wrapper = shallow(<App/>);
-    const header = shallow( <Header/>);
-    expect(wrapper.contains(header)).toEqual(true);
+    const wrapper = shallow(<App />);
+    const header = shallow(<Header/>);
+    expect(wrapper.contains(header).toEqual(true))
   });
-})
 
 
+});
